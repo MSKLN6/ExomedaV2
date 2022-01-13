8ba79dc62a674bfe9b10085e50acbbd45ad7e5ec
vando <vando@DESKTOP-UHCOTVD>
2022-01-13 16:10:22 +0100

    beweging verbeterd van scherm smoothhh

--- src/main/java/be/fiiw/exomeda/BeweegEntity.java
+++ src/main/java/be/fiiw/exomeda/BeweegEntity.java
@@ -24,6 +24,5 @@ public class BeweegEntity extends TimerTask{
     public void run() {
         model.tick();
         this.controller.update();
-        Platform.runLater(controller::update);
     }
 }
