c1928bd12cdeb25fc11d6b28393a515e1148703d
vando <vando@DESKTOP-UHCOTVD>
2022-01-13 15:37:14 +0100

    betere achtergrond transitie

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -9,5 +9,74 @@ package model;
  * @author DaanB
  */
 public class Player {
+    int health;
+    protected Vector position;
+    private Vector velocity;
+    private Vector direction;
+    private Vector speedAmount;
     
+    
+    public Player() {
+        this.health = 3;
+        this.position = new Vector (400,400);
+        this.velocity = new Vector(0,0);
+        this.direction = new Vector(-1,0);
+        this.speedAmount = new Vector(1,1);
+    }
+
+    public Player(int health, Vector position, Vector velocity, Vector direction, Vector speedAmount) {
+        this.health = health;
+        this.position = position;
+        this.velocity = velocity;
+        this.direction = direction;
+        this.speedAmount = speedAmount;
+    }
+    
+    
+    
+    
+    public void update() {
+        this.position.x += this.velocity.x * this.speedAmount.x;
+        this.position.y += this.velocity.y * this.speedAmount.y;
+    }
+    
+    public Vector getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector position) {
+        this.position = position;
+    }
+    
+    public Vector getDirection() {
+        return direction;
+    }
+
+    public void getDirection(Vector direction) {
+        this.direction = direction;
+    }
+    
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    public Vector getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector velocity) {
+        this.velocity = velocity;
+    }
+    
+    public Vector getSpeedAmount() {
+        return speedAmount;
+    }
+
+    public void setSpeedAmount(Vector speedAmount) {
+        this.speedAmount = speedAmount;
+    }
 }
--- src/main/java/view/PlayerView.java
+++ src/main/java/view/PlayerView.java
@@ -4,7 +4,12 @@
  */
 package view;
 
+import be.fiiw.exomeda.ImageController;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import model.LoadedImage;
 import model.Player;
 
 /**
@@ -13,9 +18,38 @@ import model.Player;
  */
 public class PlayerView extends Region{
     private Player model;
+    private static int PLAYER_WIDTH = 60;
+    private static int PLAYER_HEIGHT = 60;
 
     public PlayerView(Player model) {
         
+        this.model = model;
+        this.tekenPlayer();
+    }
+
+    public Player getModel() {
+        return model;
+    }
+    
+    public void update() {
+        this.setTranslateX( this.model.getPosition().x );
+        this.setTranslateY( this.model.getPosition().y );
+    }
+    
+    public void tekenPlayer() {
+        
+        Image image = null;
+        image = ImageController.getImage(LoadedImage.Type.PLAYER);
+        
+        // https://docs.oracle.com/javase/8/javafx/api/javafx/scene/image/ImageView.html
+        ImageView iv = new ImageView( image );
+        iv.setFitWidth( PLAYER_WIDTH - 5);
+        iv.setPreserveRatio(true);
+        iv.setSmooth(true);
+        
+        this.getChildren().add(iv);
+        
+        this.update();
     }
     
     
